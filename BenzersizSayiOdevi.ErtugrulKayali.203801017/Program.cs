968eeac338010c9832e49998bad3992d74b04b2f
Ertuğrul KAYALI <ertugrul.kayali@gazi.edu.tr>
2021-03-06 14:09:17 +0300

    Proje dosyası ekle.

---
+++ BenzersizSayiOdevi.ErtugrulKayali.203801017/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenzersizSayiOdevi.ErtugrulKayali._203801017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kaç Adet Sayı Üretmek İstersiniz? :");
+            int adet = int.Parse(Console.ReadLine());
+            int[] sayilar = new int[adet];
+
+            Random rnd = new Random();
+            bool esitmi = true;
+
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                esitmi = true;
+                while (esitmi)
+                {
+                    int sayi = rnd.Next(1, 100);
+                    bool esitOlanVarMi = false;
+                    for (int j = 0; j < sayilar.Length; j++)
+                    {
+                        if (sayilar[j] == sayi)
+                        {
+                            esitOlanVarMi = true;
+                            break;
+                        }
+                    }
+                    if (!esitOlanVarMi)
+                    {
+                        sayilar[i] = sayi;
+                        esitmi = false;
+                    }
+                }
+            }
+            Array.Sort(sayilar);
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.Write($"{sayilar[i]}-");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
